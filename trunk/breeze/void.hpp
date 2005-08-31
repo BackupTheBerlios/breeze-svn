f527fd46548ef7bd159f0e45d9198ce2086e2ce9
biochimia <biochimia@50a9366a-59ff-0310-814c-fa73681ed52a>
2005-08-31 04:08:57 +0000

    breeze::void_ and incomplete type

---
+++ trunk/breeze/void.hpp
@@ -0,0 +1,18 @@
+//
+//  Copyright (c) 2004 João Abecasis
+//
+//  Distributed under the Boost Software License, Version 1.0. (See
+//  accompanying file LICENSE_1_0.txt or copy at
+//  http://www.boost.org/LICENSE_1_0.txt)
+//
+
+#if !defined(BREEZE_VOID_HPP_INCLUDED)
+#define BREEZE_VOID_HPP_INCLUDED
+
+namespace breeze {
+
+    struct void_;
+
+} // namespace breeze
+
+#endif // include guard
