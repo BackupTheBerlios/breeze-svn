99bd64aa6c6898d014e793027ba86637afec1705
biochimia <biochimia@50a9366a-59ff-0310-814c-fa73681ed52a>
2005-09-12 17:56:26 +0000

    add breeze::scoped_array

---
+++ trunk/breeze/scoped_array.hpp
@@ -0,0 +1,95 @@
+//
+//  Copyright (c) 2005 João Abecasis
+//
+//  Distributed under the Boost Software License, Version 1.0. (See
+//  accompanying file LICENSE_1_0.txt or copy at
+//  http://www.boost.org/LICENSE_1_0.txt)
+//
+
+#if !defined(BREEZE_SCOPED_ARRAY_HPP_INCLUDED)
+#define BREEZE_SCOPED_ARRAY_HPP_INCLUDED
+
+#include <breeze/scoped_ptr.hpp>
+#include <breeze/detail/trivial_iterator_tag.hpp>
+
+#include <boost/iterator/iterator_traits.hpp>
+
+#include <iterator>
+
+namespace breeze {
+
+    namespace detail {
+
+        struct scoped_array_deleter
+        {
+            template <class T>
+            void operator()(T * p)
+            {
+                delete [] p;
+            }
+        };
+
+    } // namespace detail
+
+    template <
+        class T,
+        class Pointer = default_,
+        class Reference = default_,
+        class Deleter = default_
+    >
+    struct scoped_array
+        : scoped_ptr<T, Pointer, Reference, typename filter_default<Deleter,
+            detail::scoped_array_deleter>::type >
+    {
+    private:
+        typedef scoped_ptr<T, Pointer, Reference, typename filter_default<
+            Deleter, detail::scoped_array_deleter>::type > scoped_ptr_base_;
+
+    public:
+        typedef typename scoped_ptr_base_::pointer pointer;
+        typedef typename scoped_ptr_base_::reference reference;
+        typedef typename boost::iterator_difference<pointer>::type
+            difference_type;
+        typedef typename scoped_ptr_base_::deleter_type deleter_type;
+
+    private:
+        friend struct std::iterator_traits<scoped_array>;
+
+        typedef T value_type;
+        typedef detail::trivial_iterator_tag iterator_category;
+
+    public:
+        explicit scoped_array(
+            pointer p = pointer(),
+            deleter_type const & d = deleter_type()
+        )
+            : scoped_ptr_base_(p, d)
+        {
+        }
+
+        reference operator[](difference_type i) const
+        {
+            return this->get()[i];
+        }
+    };
+
+    ////////////////////////////////////////////////////////////////////////////
+    template <class T, class P, class R, class D>
+    void swap(scoped_array<T, P, R, D> & a, scoped_array<T, P, R, D> & b)
+    {
+        a.swap(b);
+    }
+
+} // namespace breeze
+
+namespace boost {
+
+    template <class T, class P, class R, class D>
+    struct indirect_reference<breeze::scoped_array<T, P, R, D> >
+    {
+        typedef typename breeze::scoped_array<T, P, R, D>::reference reference;
+    };
+
+} // namespace boost
+
+#endif // include guard
